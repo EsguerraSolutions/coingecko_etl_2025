7096a7d368f5e7fa1e1a57ff0808a724eb2677ec
esguerrasolutions <esguerradesign@gmail.com>
2025-11-06 22:20:00 +0800

    removed test.py

--- test_spark.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
-print("Spark version:", spark.version)
-spark.range(5).show()
-spark.stop()
