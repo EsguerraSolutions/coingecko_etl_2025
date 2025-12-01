9cbfb0e0920094221b0248a7c83933729e03ef46
esguerrasolutions <esguerradesign@gmail.com>
2025-12-01 19:01:24 +0800

    fixed silver layer cleaning function for market cap rankings

--- notebooks/cleaning_functions/rank_clean.py
+++ notebooks/cleaning_functions/rank_clean.py
@@ -5,6 +5,6 @@ def rank_clean(df, column_name, rank_column_name):
 
     w = Window.orderBy(F.col(column_name).desc())
 
-    df_with_cleaned_rank = df.withColumn(column_name, F.row_number().over(w))
+    df_with_cleaned_rank = df.withColumn(rank_column_name, F.row_number().over(w))
 
     return df_with_cleaned_rank
